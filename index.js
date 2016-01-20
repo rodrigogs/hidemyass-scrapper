01aa9625b78c4156d0f0e77767ae6103fdeda7ac
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2016-01-20 13:04:38 +0000

    fixed loading ip addresses before the page is fully loaded

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const phantom = require('phantom');
 const async = require('async');
+const validator = require('validator');
 
 const URL = 'http://proxylist.hidemyass.com/search-1292985#listable';
 
@@ -122,66 +123,65 @@ module.exports = {
             },
 
             (ph, page, cb) => {
-                page.evaluate(
-                    /* It runs on the virtual browser, so we cant use ES6 */
-                    function () {
-                        var gtws = [];
-                        var table = document.querySelector('table#listable tbody');
-
-                        if (table) {
-                            var rows = document.querySelectorAll('tr', table);
-
-                            for (var i = 0, len = rows.length; i < len; i++) {
-                                var tr = rows[i];
-
-                                var gateway = {};
-                                var cols = document.querySelectorAll('td', tr);
-
-                                for (var x = 0, xlen = rows.length; x < xlen; x++) {
-
-                                    var col = cols[x];
-                                    switch (col.cellIndex) {
-                                        case 0:
-                                            gateway.lastUpdate = col.innerText.trim();
-                                            break;
-                                        case 1:
-                                            gateway.hostname = col.innerText.trim();
-                                            break;
-                                        case 2:
-                                            gateway.port = col.innerText.trim();
-                                            break;
-                                        case 3:
-                                            gateway.country = col.innerText.trim();
-                                            break;
-                                        case 6:
-                                            gateway.protocol = col.innerText.trim().toLowerCase();
-                                            break;
-                                        case 7:
-                                            gateway.anonymity = col.innerText.trim();
-                                            break;
-                                    }
-                                }
-
-                                gateway.provider = 'HideMyAss';
-                                gtws.push(gateway);
-                            }
-                        }
-
-                        return gtws;
-                    }
-                    /* XXX */
-                    , gateways => {
-                        gateways = gateways
-                            .filter(n => {
-                                return !!n
-                                    && !!n.hostname
-                                    && !!n.port
-                                    && !!n.protocol;
-                            });
+                setTimeout(function () {
+                    page.evaluate(
+                        /* It runs on the virtual browser, so we cant use ES6 */
+                        function () {
+                            var gtws = [];
+                            var table = $('table#listable tbody');
+                            if (table) {
     
-                        cb(null, gateways, ph);
-                    }
-                );
+                                var rows = table.find('tr');
+                                rows.each(function (index, tr) {
+    
+                                    var gateway = {};
+                                    var cols = $(tr).find('td');
+                                    cols.each(function (index, col) {
+                                        col = $(col);
+                                        switch (col.index()) {
+                                            case 0:
+                                                gateway.lastUpdate = col[0].innerText.trim();
+                                                break;
+                                            case 1:
+                                                gateway.hostname = col[0].innerText.trim();
+                                                break;
+                                            case 2:
+                                                gateway.port = col[0].innerText.trim();
+                                                break;
+                                            case 3:
+                                                gateway.country = col[0].innerText.trim();
+                                                break;
+                                            case 6:
+                                                gateway.protocol = col[0].innerText.trim().toLowerCase();
+                                                break;
+                                            case 7:
+                                                gateway.anonymity = col[0].innerText.trim();
+                                                break;
+                                        }
+                                    });
+    
+                                    gateway.provider = 'HideMyAss';
+                                    gtws.push(gateway);
+                                });
+                            }
+    
+                            return gtws;
+                        }
+                        /* XXX */
+                        , gateways => {
+                            gateways = gateways
+                                .filter(n => {
+                                    return n
+                                        && n.hostname
+                                        && validator.isIP(n.hostname)
+                                        && n.port
+                                        && n.protocol;
+                                });
+
+                            cb(null, gateways, ph);
+                        }
+                    );
+                }, 1000);
             }
         ], (err, gateways, ph) => {
             if (ph) {
