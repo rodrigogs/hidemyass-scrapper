722b6f121e9eb8ff7b20836a8c9beed86fd6f735
rodrigo.smscom@gmail.com <rgs84060989>
2016-08-30 16:54:33 -0300

    Updated

--- index.js
+++ index.js
@@ -3,198 +3,225 @@
 const phantom = require('phantom');
 const async = require('async');
 const validator = require('validator');
+const Promise = require('bluebird');
 
 const URL = 'http://proxylist.hidemyass.com/search-1292985#listable';
+
+/**
+ * Loads a new page.
+ *
+ * @param {Object} options
+ * @returns {Promise}
+ */
+function _loadPage(options) {
+  options = options || {};
+
+  return new Promise((resolve, reject) => {
+    let instance,
+      page;
+
+    phantom.create(['--ignore-ssl-errors=yes', '--load-images=no', '--cookies-file=cookies.txt'])
+      .then(ph => {
+        instance = ph;
+        return instance.createPage();
+      })
+      .then(createdPage => {
+        page = createdPage;
+        if (options.proxy) {
+          return page.setProxy(options.proxy);
+        } else {
+          return Promise.resolve();
+        }
+      })
+      .then(() => page.open(options.url || URL))
+      .then(status => {
+        if (status !== 'success') {
+          throw new Error(`Error opening page for ${URL}`, null, ph);
+        }
+        return Promise.resolve();
+      })
+      .then(() => {
+        let ret = {instance: instance, page: page};
+        ret.exit = () => ret.instance.exit();
+        return Promise.resolve(ret);
+      })
+      .then(_verifyDisponibility)
+      .then(resolve)
+      .catch(err => {
+        instance.exit();
+        reject(err);
+      });
+  });
+}
+
+/**
+ *
+ * @param loaded
+ */
+function _verifyDisponibility(loaded) {
+  return new Promise((resolve, reject) => {
+    loaded.page.evaluate(function () {
+      var el = $('body');
+      if (el.html().indexOf('403 Forbidden') > -1) {
+        return 'forbidden';
+      }
+      if (el.html().indexOf('recaptcha_challenge_field') > -1) {
+        return 'captcha';
+      }
+    }).then(status => {
+      if (status === 'forbidden') {
+        throw new Error('Your ip is blacklisted for proxylist.hidemyass.com');
+      }
+      if (status === 'captcha') {
+        throw new Error('Captcha detected!');
+      }
+      resolve(loaded);
+    }).catch(reject);
+  });
+}
+
+/**
+ * Retrieve the current pagination max number.
+ *
+ * @param {Object} options
+ * @param {Function} callback
+ * @returns {Promise}
+ */
+function _getPages(options, callback) {
+  options = options || {};
+
+  if (options instanceof Function) {
+    callback = options;
+    options = {};
+  }
+
+  return new Promise((resolve, reject) => {
+    _loadPage(options).then(loaded => {
+      loaded.page.evaluate(function () {
+        var paginationSection = $('section.hma-pagination'),
+          pages = $('.pagination li', paginationSection).not('.arrow');
+
+        return pages.length;
+      }).then(pages => {
+        loaded.exit();
+        if (callback) {
+          callback(null, pages);
+        }
+        resolve(pages);
+      });
+
+    }).catch(err => {
+      if (callback) {
+        return callback(err);
+      }
+      reject(err);
+    });
+  });
+}
+
+/**
+ * Crawl proxylist.hidemyass.com and retrieve a list of proxy servers.
+ *
+ * @param {Object} options
+ * @param {Function} callback function (gateways) {}
+ * @returns {Promise} Object[]
+ *          lastUpdate
+ *          hostname
+ *          port
+ *          country
+ *          protocol
+ *          anonymity
+ */
+function _crawl(options, callback) {
+  options = options || {};
+
+  if (options instanceof Function) {
+    callback = options;
+    options = {};
+  }
+
+  options.url = options.page ? `${URL}?page=${options.page}` : URL;
+
+  return new Promise((resolve, reject) => {
+    _loadPage(options).then(loaded => {
+
+      return new Promise((resolve, reject) => {
+        setTimeout(function () {
+          loaded.page.evaluate(function () {
+            var gtws = [];
+            var table = $('table#listable tbody');
+            if (table) {
+
+              var rows = table.find('tr');
+              rows.each(function (index, tr) {
+
+                var gateway = {};
+                var cols = $(tr).find('td');
+                cols.each(function (index, col) {
+                  col = $(col);
+                  switch (col.index()) {
+                    case 0:
+                      gateway.lastUpdate = col[0].innerText.trim();
+                      break;
+                    case 1:
+                      gateway.hostname = col[0].innerText.trim();
+                      break;
+                    case 2:
+                      gateway.port = col[0].innerText.trim();
+                      break;
+                    case 3:
+                      gateway.country = col[0].innerText.trim();
+                      break;
+                    case 6:
+                      gateway.protocol = col[0].innerText.trim().toLowerCase();
+                      break;
+                    case 7:
+                      gateway.anonymity = col[0].innerText.trim();
+                      break;
+                  }
+                });
+
+                gateway.provider = 'HideMyAss';
+                gtws.push(gateway);
+              });
+            }
+
+            return gtws;
+          })
+            .then(resolve)
+            .catch(reject);
+        }, 1500);
+
+      }).then(gateways => {
+        gateways = (gateways || [])
+          .filter(n => {
+            return n
+              && n.hostname
+              && validator.isIP(n.hostname)
+              && n.port
+              && n.protocol;
+          });
+
+        loaded.exit();
+        if (callback) {
+          callback(null, gateways);
+        }
+        resolve(gateways);
+      });
+
+    }).catch(err => {
+      if (callback) {
+        return callback(err);
+      }
+      reject(err);
+    });
+  });
+}
 
 /**
  * @module HideMyAssProvider
  */
 module.exports = {
-
-    /**
-     * Retrieve the current pagination max number.
-     * 
-     * @param {Object} options
-     * @param {Function} callback
-     * @returns {Number}
-     */
-    getPages: (options, callback) => {
-        options = options || {};
-        
-        if (options instanceof Function) {
-            callback = options;
-            options = {};
-        }
-
-        const url = options.url || URL;
-
-        async.waterfall([
-            (cb) => {
-                const args = options.proxy ? {
-                    parameters: {
-                        proxy: options.proxy
-                    }
-                } : {};
-
-                phantom.create(args, ph => cb(null, ph) );
-            },
-
-            (ph, cb) => {
-                ph.createPage(page => cb(null, ph, page) );
-            },
-
-            (ph, page, cb) => {
-                page.open(url, status => {
-                    if (status !== 'success') {
-                        return cb(new Error(`Error opening page for ${url}`), null, ph);
-                    }
-                    cb(null, ph, page);
-                });
-            },
-
-            (ph, page, cb) => {
-                page.evaluate(
-                    /* It runs on the virtual browser, so we cant use ES6 */
-                        function () {
-                            var paginationSection = $('section.hma-pagination'),
-                                pages = $('.pagination li', paginationSection).not('.arrow');
-
-                            return pages.length;
-                        }
-                    /* XXX */
-                    , pages => {
-                        cb(null, pages, ph);
-                    }
-                );
-            }
-        ], (err, pages, ph) => {
-            if (ph) {
-                ph.exit();
-            }
-            if (err) {
-                return callback(err);
-            }
-            callback(null, pages);
-        });
-    },
-
-    /**
-     * Crawl proxylist.hidemyass.com and retrieve a list of proxy servers.
-     * 
-     * @param {Object} options
-     * @param {Function} callback function (gateways) {}
-     * @returns Object[] 
-     *          lastUpdate
-     *          hostname
-     *          port
-     *          country
-     *          protocol
-     *          anonymity
-     */
-    crawl: (options, callback) => {
-        options = options || {};
-        
-        if (options instanceof Function) {
-            callback = options;
-            options = {};
-        }
-
-        let url = options.url || URL;
-        const PAGE_URL = options.page ? url.replace('#', `/${options.page}#`) : url;
-
-        async.waterfall([
-            (cb) => {
-                const args = options.proxy ? {
-                    parameters: {
-                        proxy: options.proxy
-                    }
-                } : {};
-
-                phantom.create(args, ph => cb(null, ph) );
-            },
-
-            (ph, cb) => {
-                ph.createPage(page => cb(null, ph, page) );
-            },
-
-            (ph, page, cb) => {
-                page.open(url, status => {
-                    if (status !== 'success') {
-                        return cb(new Error(`Error opening page for ${url}`, null, ph));
-                    }
-                    cb(null, ph, page);
-                });
-            },
-
-            (ph, page, cb) => {
-                setTimeout(function () {
-                    page.evaluate(
-                        /* It runs on the virtual browser, so we cant use ES6 */
-                        function () {
-                            var gtws = [];
-                            var table = $('table#listable tbody');
-                            if (table) {
-    
-                                var rows = table.find('tr');
-                                rows.each(function (index, tr) {
-    
-                                    var gateway = {};
-                                    var cols = $(tr).find('td');
-                                    cols.each(function (index, col) {
-                                        col = $(col);
-                                        switch (col.index()) {
-                                            case 0:
-                                                gateway.lastUpdate = col[0].innerText.trim();
-                                                break;
-                                            case 1:
-                                                gateway.hostname = col[0].innerText.trim();
-                                                break;
-                                            case 2:
-                                                gateway.port = col[0].innerText.trim();
-                                                break;
-                                            case 3:
-                                                gateway.country = col[0].innerText.trim();
-                                                break;
-                                            case 6:
-                                                gateway.protocol = col[0].innerText.trim().toLowerCase();
-                                                break;
-                                            case 7:
-                                                gateway.anonymity = col[0].innerText.trim();
-                                                break;
-                                        }
-                                    });
-    
-                                    gateway.provider = 'HideMyAss';
-                                    gtws.push(gateway);
-                                });
-                            }
-    
-                            return gtws;
-                        }
-                        /* XXX */
-                        , gateways => {
-                            gateways = (gateways || [])
-                                .filter(n => {
-                                    return n
-                                        && n.hostname
-                                        && validator.isIP(n.hostname)
-                                        && n.port
-                                        && n.protocol;
-                                });
-
-                            cb(null, gateways, ph);
-                        }
-                    );
-                }, 1000);
-            }
-        ], (err, gateways, ph) => {
-            if (ph) {
-                ph.exit();
-            }
-            if (err) {
-                return callback(err);
-            }
-            callback(null, gateways);
-        });
-    }
+  getPages: _getPages,
+  crawl: _crawl
 };
--- tests.js
+++ tests.js
@@ -1,35 +1,25 @@
 import test from 'ava';
 import HideMyAss from './index';
 
-test.cb('get pagination number', t => {
-    t.plan(2);
-    HideMyAss.getPages({}, (err, pages) => {
-        t.ifError(err, 'Unexpected error getting pagination');
-        t.is(typeof pages, 'number', 'Pages from getPages must be a number');
-        t.end();
-    });
+test('get pagination number', t => {
+  return HideMyAss.getPages().then(pages => {
+    t.is(typeof pages, 'number', 'Pages from getPages must be a number');
+  });
 });
 
-test.cb('crawl the proxy list from FreeProxyLists', t => {
-    t.plan(3);
-    HideMyAss.crawl((err, gateways) => {
-        t.ifError(err, 'Unexpected error crawling page');
-        t.true((gateways instanceof Array), 'Return should be an array');
-        t.true((gateways.length > 0), 'Return should have values');
-        t.end();
-    });
+test('crawl the proxy list from HideMyAss', t => {
+  return HideMyAss.crawl().then(gateways => {
+    console.log(gateways);
+    t.true((gateways instanceof Array), 'Return should be an array');
+  });
 });
 
-test.cb('crawl the proxy list from FreeProxyLists passing a specific page number', t => {
-    t.plan(4);
-    HideMyAss.getPages((err, pages) => {
-        t.ifError(err, 'Unexpected error getting pagination');
-        t.is(typeof pages, 'number', 'GetPages return type should be a number');
+test('crawl the proxy list from HideMyAss passing a specific page number', t => {
+  return HideMyAss.getPages().then(pages => {
+    t.is(typeof pages, 'number', 'GetPages return type should be a number');
 
-        HideMyAss.crawl(pages, (err, gateways) => {
-            t.ifError(err, 'Unexpected error crawling page');
-            t.true((gateways instanceof Array), 'Crawl return type should be an array');
-            t.end();
-        });
+    return HideMyAss.crawl({page: pages}).then(gateways => {
+      t.true((gateways instanceof Array), 'Crawl return type should be an array');
     });
+  });
 });
